a39edca1bfa6e6bf069894d650753feb1fab465a
admin <869570838@qq.com>
2017-10-13 11:20:05 +0800

    hahah

--- src/main/java/test/Stream.java
+++ src/main/java/test/Stream.java
@@ -1,21 +1,21 @@
-package test;
-
-/**
- * Created by admin on 2017/8/3.
- */
-public class Stream extends Root {
-
-    public Stream() {
-        System.out.println("这是Stream 。");
-    }
-
-    @Override
-    public void gogogo(Root root) {
-        System.out.println("Stream=-------gogogogo");
-    }
-
-    public static void main(String[] arg) {
-        Stream stream = new Stream();
-        stream.gogogo(stream);
-    }
-}
+//package test;
+//
+///**
+// * Created by admin on 2017/8/3.
+// */
+//public class Stream extends Root {
+//
+//    public Stream() {
+//        System.out.println("这是Stream 。");
+//    }
+//
+//    @Override
+//    public void gogogo(Root root) {
+//        System.out.println("Stream=-------gogogogo");
+//    }
+//
+//    public static void main(String[] arg) {
+//        Stream stream = new Stream();
+//        stream.gogogo(stream);
+//    }
+//}
